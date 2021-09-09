d2f0cf5967a7fd2fce990a4d057dc8ffcda326ea
mprahl <mprahl@users.noreply.github.com>
2021-09-09 13:25:07 -0400

    Add support for configuring the plugin
    The configuration comes from an input PolicyGenerator configuration
    YAML file.
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

---
+++ internal/plugin.go
@@ -0,0 +1,227 @@
+// Copyright Contributors to the Open Cluster Management project
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type manifest struct {
+	Path string `json:"path,omitempty" yaml:"path,omitempty"`
+}
+
+type namespaceSelector struct {
+	Exclude []string `json:"exclude,omitempty" yaml:"exclude,omitempty"`
+	Include []string `json:"include,omitempty" yaml:"include,omitempty"`
+}
+
+// policyConfig represents a policy entry in the PolicyGenerator configuration.
+type policyConfig struct {
+	Categories     []string `json:"categories,omitempty" yaml:"categories,omitempty"`
+	ComplianceType string   `json:"complianceType,omitempty" yaml:"complianceType,omitempty"`
+	Controls       []string `json:"controls,omitempty" yaml:"controls,omitempty"`
+	Disabled       bool     `json:"disabled,omitempty" yaml:"disabled,omitempty"`
+	// Make this a slice of structs in the event we want additional configuration related to
+	// a manifest such as accepting patches.
+	Manifests         []manifest        `json:"manifests,omitempty" yaml:"manifests,omitempty"`
+	Name              string            `json:"name,omitempty" yaml:"name,omitempty"`
+	NamespaceSelector namespaceSelector `json:"namespaceSelector,omitempty" yaml:"namespaceSelector,omitempty"`
+	// This is named Placement so that eventually PlacementRules and Placements will be supported
+	Placement struct {
+		ClusterSelectors  map[string]string `json:"clusterSelectors,omitempty" yaml:"clusterSelectors,omitempty"`
+		PlacementRulePath string            `json:"placementRulePath,omitempty" yaml:"placementRulePath,omitempty"`
+	} `json:"placement,omitempty" yaml:"placement,omitempty"`
+	RemediationAction string   `json:"remediationAction,omitempty" yaml:"remediationAction,omitempty"`
+	Severity          string   `json:"severity,omitempty" yaml:"severity,omitempty"`
+	Standards         []string `json:"standards,omitempty" yaml:"standards,omitempty"`
+}
+
+// Plugin is used to store the PolicyGenerator configuration and the methods to generate the
+// desired policies.
+type Plugin struct {
+	Metadata struct {
+		Name string `json:"name,omitempty" yaml:"name,omitempty"`
+	} `json:"metadata,omitempty" yaml:"metadata,omitempty"`
+	PlacementBindingDefaults struct {
+		Name string `json:"name,omitempty" yaml:"name,omitempty"`
+	} `json:"placementBindingDefaults,omitempty" yaml:"placementBindingDefaults,omitempty"`
+	PolicyDefaults struct {
+		Categories        []string          `json:"categories,omitempty" yaml:"categories,omitempty"`
+		ComplianceType    string            `json:"complianceType,omitempty" yaml:"complianceType,omitempty"`
+		Controls          []string          `json:"controls,omitempty" yaml:"controls,omitempty"`
+		Namespace         string            `json:"namespace,omitempty" yaml:"namespace,omitempty"`
+		NamespaceSelector namespaceSelector `json:"namespaceSelector,omitempty" yaml:"namespaceSelector,omitempty"`
+		// This is named Placement so that eventually PlacementRules and Placements will be supported
+		Placement struct {
+			ClusterSelectors  map[string]string `json:"clusterSelectors,omitempty" yaml:"clusterSelectors,omitempty"`
+			PlacementRulePath string            `json:"placementRulePath,omitempty" yaml:"placementRulePath,omitempty"`
+		} `json:"placement,omitempty" yaml:"placement,omitempty"`
+		RemediationAction string   `json:"remediationAction,omitempty" yaml:"remediationAction,omitempty"`
+		Severity          string   `json:"severity,omitempty" yaml:"severity,omitempty"`
+		Standards         []string `json:"standards,omitempty" yaml:"standards,omitempty"`
+	} `json:"policyDefaults,omitempty" yaml:"policyDefaults,omitempty"`
+	Policies []policyConfig `json:"policies" yaml:"policies"`
+}
+
+// Config validates the input PolicyGenerator configuration, applies any missing defaults, and
+// configures the Policy object.
+func (p *Plugin) Config(config []byte) error {
+	err := yaml.Unmarshal(config, p)
+	if err != nil {
+		return fmt.Errorf("the PolicyGenerator configuration file is invalid: %w", err)
+	}
+	p.applyDefaults()
+
+	return p.assertValidConfig()
+}
+
+// applyDefaults applies any missing defaults under Policy.PlacementBindingDefaults and
+// Policy.PolicyDefaults. It then applies the defaults and user provided defaults on each
+// policy entry if they are not overridden by the user.
+func (p *Plugin) applyDefaults() {
+	if len(p.Policies) == 0 {
+		return
+	}
+
+	// Set defaults to the defaults that aren't overridden
+	if p.PlacementBindingDefaults.Name == "" && len(p.Policies) == 1 {
+		p.PlacementBindingDefaults.Name = "binding-" + p.Policies[0].Name
+	}
+
+	if p.PolicyDefaults.Categories == nil {
+		p.PolicyDefaults.Categories = []string{"CM Configuration Management"}
+	}
+
+	if p.PolicyDefaults.ComplianceType == "" {
+		p.PolicyDefaults.ComplianceType = "musthave"
+	}
+
+	if p.PolicyDefaults.Controls == nil {
+		p.PolicyDefaults.Controls = []string{"CM-2 Baseline Configuration"}
+	}
+
+	if p.PolicyDefaults.RemediationAction == "" {
+		p.PolicyDefaults.RemediationAction = "inform"
+	}
+
+	if p.PolicyDefaults.Severity == "" {
+		p.PolicyDefaults.Severity = "low"
+	}
+
+	if p.PolicyDefaults.Standards == nil {
+		p.PolicyDefaults.Standards = []string{"NIST SP 800-53"}
+	}
+
+	for i := range p.Policies {
+		policy := &p.Policies[i]
+		if policy.Categories == nil {
+			policy.Categories = p.PolicyDefaults.Categories
+		}
+
+		if policy.ComplianceType == "" {
+			policy.ComplianceType = p.PolicyDefaults.ComplianceType
+		}
+
+		if policy.Controls == nil {
+			policy.Controls = p.PolicyDefaults.Controls
+		}
+
+		// If both cluster selectors and placement rule path aren't set, then use the
+		// defaults with a priority on placement rule path.
+		if len(policy.Placement.ClusterSelectors) == 0 && policy.Placement.PlacementRulePath == "" {
+			if p.PolicyDefaults.Placement.PlacementRulePath != "" {
+				policy.Placement.PlacementRulePath = p.PolicyDefaults.Placement.PlacementRulePath
+			} else if len(p.PolicyDefaults.Placement.ClusterSelectors) > 0 {
+				policy.Placement.ClusterSelectors = p.PolicyDefaults.Placement.ClusterSelectors
+			}
+		}
+
+		// Only use defaults when when both include and exclude are not set on the policy
+		nsSelector := policy.NamespaceSelector
+		defNsSelector := p.PolicyDefaults.NamespaceSelector
+		if nsSelector.Exclude == nil && nsSelector.Include == nil {
+			policy.NamespaceSelector = defNsSelector
+		}
+
+		if policy.RemediationAction == "" {
+			policy.RemediationAction = p.PolicyDefaults.RemediationAction
+		}
+
+		if policy.Severity == "" {
+			policy.Severity = p.PolicyDefaults.Severity
+		}
+
+		if policy.Standards == nil {
+			policy.Standards = p.PolicyDefaults.Standards
+		}
+	}
+}
+
+// assertValidConfig verifies that the user provided configuration has all the
+// required fields. Note that this should be run only after applyDefaults is run.
+func (p *Plugin) assertValidConfig() error {
+	if p.PlacementBindingDefaults.Name == "" && len(p.Policies) > 1 {
+		return errors.New(
+			"placementBindingDefaults.name must be set when there are mutiple policies",
+		)
+	}
+
+	if p.PolicyDefaults.Namespace == "" {
+		return errors.New("policyDefaults.namespace is empty but it must be set")
+	}
+
+	if len(p.Policies) == 0 {
+		return errors.New("policies is empty but it must be set")
+	}
+
+	seen := map[string]bool{}
+	for i := range p.Policies {
+		policy := &p.Policies[i]
+		if len(policy.Placement.ClusterSelectors) != 0 && policy.Placement.PlacementRulePath != "" {
+			return errors.New(
+				"a policy may not specify placement.clusterSelectors and " +
+					"placement.placementRulePath together",
+			)
+		}
+
+		if len(policy.Manifests) == 0 {
+			return errors.New("each policy must have at least one manifest")
+		}
+
+		for _, manifest := range policy.Manifests {
+			if manifest.Path == "" {
+				return errors.New("each policy manifest entry must have path set")
+			}
+
+			_, err := os.Stat(manifest.Path)
+			if err != nil {
+				return fmt.Errorf("could not read the manifest path %s", manifest.Path)
+			}
+		}
+
+		if policy.Name == "" {
+			return errors.New("each policy must have a name set")
+		}
+
+		if seen[policy.Name] {
+			return fmt.Errorf("each policy must have a unique name set: %s", policy.Name)
+		}
+
+		if policy.Placement.PlacementRulePath != "" {
+			_, err := os.Stat(policy.Placement.PlacementRulePath)
+			if err != nil {
+				return fmt.Errorf(
+					"could not read the placement rule path %s",
+					policy.Placement.PlacementRulePath,
+				)
+			}
+		}
+
+		seen[policy.Name] = true
+	}
+
+	return nil
+}
---
+++ internal/plugin_config_test.go
@@ -0,0 +1,444 @@
+// Copyright Contributors to the Open Cluster Management project
+package internal
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"testing"
+)
+
+func createConfigMap(t *testing.T, tmpDir, filename string) {
+	t.Helper()
+	manifestsPath := path.Join(tmpDir, filename)
+	yamlContent := `
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: my-configmap
+data:
+  game.properties: enemies=potato
+`
+	err := ioutil.WriteFile(manifestsPath, []byte(yamlContent), 0o666)
+	if err != nil {
+		t.Fatalf("Failed to write %s", manifestsPath)
+	}
+}
+
+func TestConfig(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	createConfigMap(t, tmpDir, "configmap.yaml")
+	configMapPath := path.Join(tmpDir, "configmap.yaml")
+	createConfigMap(t, tmpDir, "configmap2.yaml")
+	configMapPath2 := path.Join(tmpDir, "configmap.yaml")
+	exampleConfig := fmt.Sprintf(
+		`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+placementBindingDefaults:
+  name: my-placement-binding
+policyDefaults:
+  controls: 
+    - PR.DS-1 Data-at-rest
+  namespace: my-policies
+  namespaceSelector:
+    include:
+      - default
+    exclude:
+      - my-protected-ns
+  placement:
+    clusterSelectors:
+      cloud: red hat
+  remediationAction: enforce
+  severity: medium
+policies:
+- name: policy-app-config
+  disabled: false
+  manifests:
+    - path: %s
+  namespaceSelector:
+    include:
+      - app-ns
+  remediationAction: inform
+- name: policy-app-config2
+  disabled: true
+  manifests:
+    - path: %s
+  placement:
+    clusterSelectors:
+      cloud: weather
+`,
+		configMapPath,
+		configMapPath2,
+	)
+
+	p := Plugin{}
+	err := p.Config([]byte(exampleConfig))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assertEqual(t, p.Metadata.Name, "policy-generator-name")
+	assertEqual(t, p.PlacementBindingDefaults.Name, "my-placement-binding")
+	assertReflectEqual(t, p.PolicyDefaults.Categories, []string{"CM Configuration Management"})
+	assertEqual(t, p.PolicyDefaults.ComplianceType, "musthave")
+	assertReflectEqual(t, p.PolicyDefaults.Controls, []string{"PR.DS-1 Data-at-rest"})
+	assertEqual(t, p.PolicyDefaults.Namespace, "my-policies")
+	expectedNsSelector := namespaceSelector{
+		Exclude: []string{"my-protected-ns"}, Include: []string{"default"},
+	}
+	assertReflectEqual(t, p.PolicyDefaults.NamespaceSelector, expectedNsSelector)
+	assertEqual(t, p.PolicyDefaults.Placement.PlacementRulePath, "")
+	assertReflectEqual(
+		t,
+		p.PolicyDefaults.Placement.ClusterSelectors,
+		map[string]string{"cloud": "red hat"},
+	)
+	assertEqual(t, p.PolicyDefaults.RemediationAction, "enforce")
+	assertEqual(t, p.PolicyDefaults.Severity, "medium")
+	assertReflectEqual(t, p.PolicyDefaults.Standards, []string{"NIST SP 800-53"})
+	assertEqual(t, len(p.Policies), 2)
+
+	policy1 := p.Policies[0]
+	assertReflectEqual(t, policy1.Categories, []string{"CM Configuration Management"})
+	assertEqual(t, policy1.ComplianceType, "musthave")
+	assertReflectEqual(t, policy1.Controls, []string{"PR.DS-1 Data-at-rest"})
+	assertEqual(t, policy1.Disabled, false)
+	assertEqual(t, len(policy1.Manifests), 1)
+	assertEqual(t, policy1.Manifests[0].Path, configMapPath)
+	assertEqual(t, policy1.Name, "policy-app-config")
+	p1ExpectedNsSelector := namespaceSelector{
+		Exclude: nil, Include: []string{"app-ns"},
+	}
+	assertReflectEqual(t, policy1.NamespaceSelector, p1ExpectedNsSelector)
+	assertReflectEqual(
+		t,
+		policy1.Placement.ClusterSelectors,
+		map[string]string{"cloud": "red hat"},
+	)
+	assertEqual(t, policy1.RemediationAction, "inform")
+	assertEqual(t, policy1.Severity, "medium")
+	assertReflectEqual(t, policy1.Standards, []string{"NIST SP 800-53"})
+
+	policy2 := p.Policies[1]
+	assertReflectEqual(t, policy2.Categories, []string{"CM Configuration Management"})
+	assertEqual(t, policy2.ComplianceType, "musthave")
+	assertReflectEqual(t, policy2.Controls, []string{"PR.DS-1 Data-at-rest"})
+	assertEqual(t, policy2.Disabled, true)
+	assertEqual(t, len(policy2.Manifests), 1)
+	assertEqual(t, policy2.Manifests[0].Path, configMapPath2)
+	assertEqual(t, policy2.Name, "policy-app-config2")
+	assertReflectEqual(t, policy2.NamespaceSelector, expectedNsSelector)
+	assertReflectEqual(
+		t,
+		policy2.Placement.ClusterSelectors,
+		map[string]string{"cloud": "weather"},
+	)
+	assertEqual(t, policy2.RemediationAction, "enforce")
+	assertEqual(t, policy2.Severity, "medium")
+	assertReflectEqual(t, policy2.Standards, []string{"NIST SP 800-53"})
+}
+
+func TestConfigAllDefaults(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	createConfigMap(t, tmpDir, "configmap.yaml")
+	configMapPath := path.Join(tmpDir, "configmap.yaml")
+	defaultsConfig := fmt.Sprintf(
+		`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+policies:
+- name: policy-app-config
+  manifests:
+    - path: %s
+`,
+		configMapPath,
+	)
+	p := Plugin{}
+	err := p.Config([]byte(defaultsConfig))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	assertEqual(t, p.Metadata.Name, "policy-generator-name")
+	assertEqual(t, p.PlacementBindingDefaults.Name, "binding-policy-app-config")
+	assertReflectEqual(t, p.PolicyDefaults.Categories, []string{"CM Configuration Management"})
+	assertEqual(t, p.PolicyDefaults.ComplianceType, "musthave")
+	assertReflectEqual(t, p.PolicyDefaults.Controls, []string{"CM-2 Baseline Configuration"})
+	assertEqual(t, p.PolicyDefaults.Namespace, "my-policies")
+	expectedNsSelector := namespaceSelector{Exclude: nil, Include: nil}
+	assertReflectEqual(t, p.PolicyDefaults.NamespaceSelector, expectedNsSelector)
+	assertEqual(t, p.PolicyDefaults.Placement.PlacementRulePath, "")
+	assertEqual(t, len(p.PolicyDefaults.Placement.ClusterSelectors), 0)
+	assertEqual(t, p.PolicyDefaults.RemediationAction, "inform")
+	assertEqual(t, p.PolicyDefaults.Severity, "low")
+	assertReflectEqual(t, p.PolicyDefaults.Standards, []string{"NIST SP 800-53"})
+	assertEqual(t, len(p.Policies), 1)
+
+	policy := p.Policies[0]
+	assertReflectEqual(t, policy.Categories, []string{"CM Configuration Management"})
+	assertEqual(t, policy.ComplianceType, "musthave")
+	assertReflectEqual(t, policy.Controls, []string{"CM-2 Baseline Configuration"})
+	assertEqual(t, policy.Disabled, false)
+	assertEqual(t, len(policy.Manifests), 1)
+	assertEqual(t, policy.Manifests[0].Path, configMapPath)
+	assertEqual(t, policy.Name, "policy-app-config")
+	assertReflectEqual(t, policy.NamespaceSelector, expectedNsSelector)
+	assertEqual(t, len(policy.Placement.ClusterSelectors), 0)
+	assertEqual(t, policy.Placement.PlacementRulePath, "")
+	assertEqual(t, policy.RemediationAction, "inform")
+	assertEqual(t, policy.Severity, "low")
+	assertReflectEqual(t, policy.Standards, []string{"NIST SP 800-53"})
+}
+
+func TestConfigNoPlacementBindingName(t *testing.T) {
+	t.Parallel()
+	const config = `
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+policies:
+- name: policy-app-config
+  manifests:
+    - path: input/configmap.yaml
+- name: policy-app-config2
+  manifests:
+    - path: input/configmap2.yaml
+`
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := "placementBindingDefaults.name must be set when there are mutiple policies"
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigNoNamespace(t *testing.T) {
+	t.Parallel()
+	const config = `
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policies:
+- name: policy-app-config
+  manifests:
+    - path: input/configmap.yaml
+`
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := "policyDefaults.namespace is empty but it must be set"
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigNoPolicies(t *testing.T) {
+	t.Parallel()
+	const config = `
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+`
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := "policies is empty but it must be set"
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigMultiplePlacements(t *testing.T) {
+	t.Parallel()
+	const config = `
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+policies:
+- name: policy-app-config
+  placement:
+    clusterSelectors:
+      cloud: red hat
+    placementRulePath: path/to/plr.yaml
+  manifests:
+    - path: input/configmap.yaml
+`
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := "a policy may not specify placement.clusterSelectors and " +
+		"placement.placementRulePath together"
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigDuplicateNames(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	createConfigMap(t, tmpDir, "configmap.yaml")
+	createConfigMap(t, tmpDir, "configmap2.yaml")
+	config := fmt.Sprintf(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+placementBindingDefaults:
+  name: my-pb
+policyDefaults:
+  namespace: my-policies
+policies:
+- name: policy-app-config
+  manifests:
+    - path: %s
+- name: policy-app-config
+  manifests:
+    - path: %s
+`,
+		path.Join(tmpDir, "configmap.yaml"),
+		path.Join(tmpDir, "configmap2.yaml"),
+	)
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	assertEqual(t, err.Error(), "each policy must have a unique name set: policy-app-config")
+}
+
+func TestConfigNoManifests(t *testing.T) {
+	t.Parallel()
+	const config = `
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+policies:
+- name: policy-app-config
+`
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := "each policy must have at least one manifest"
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigManifestNotFound(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	manifestPath := path.Join(tmpDir, "configmap.yaml")
+	config := fmt.Sprintf(
+		`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+policies:
+- name: policy-app-config
+  manifests:
+    - path: %s
+`,
+		manifestPath,
+	)
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := fmt.Sprintf("could not read the manifest path %s", manifestPath)
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigNoPolicyName(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	createConfigMap(t, tmpDir, "configmap.yaml")
+	config := fmt.Sprintf(
+		`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+policies:
+- manifests:
+    - path: %s
+`,
+		path.Join(tmpDir, "configmap.yaml"),
+	)
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := "each policy must have a name set"
+	assertEqual(t, err.Error(), expected)
+}
+
+func TestConfigPlrNotFound(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	createConfigMap(t, tmpDir, "configmap.yaml")
+	plrPath := path.Join(tmpDir, "plr.yaml")
+	config := fmt.Sprintf(
+		`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-name
+policyDefaults:
+  namespace: my-policies
+  placement:
+    placementRulePath: %s
+policies:
+- name: policy-app-config
+  manifests:
+    - path: %s
+`,
+		plrPath,
+		path.Join(tmpDir, "configmap.yaml"),
+	)
+	p := Plugin{}
+	err := p.Config([]byte(config))
+	if err == nil {
+		t.Fatal("Expected an error but did not get one")
+	}
+
+	expected := fmt.Sprintf("could not read the placement rule path %s", plrPath)
+	assertEqual(t, err.Error(), expected)
+}
