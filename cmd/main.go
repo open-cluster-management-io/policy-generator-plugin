ebf56c3f2b4e7a46a4800f94c599030489a70a74
Dale Haiducek <dhaiduce@redhat.com>
2021-09-10 16:04:08 -0400

    Add cmd
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"github.com/open-cluster-management/policy-generator-plugin/internal"
+	"github.com/spf13/pflag"
+)
+
+var debug = false
+
+func main() {
+	// Parse command input
+	debugFlag := pflag.Bool("debug", false, "Print the stack trace with error messages")
+	pflag.Parse()
+	debug = *debugFlag
+
+	// Collect and parse PolicyGeneratorConfig file paths
+	generators := pflag.Args()
+	var outputBuffer bytes.Buffer
+	for _, gen := range generators {
+		outputBuffer.Write(processGeneratorConfig(gen))
+	}
+
+	// Output results to stdout for Kustomize to handle
+	fmt.Print(outputBuffer.String())
+}
+
+// Error handler
+func errorAndExit(msg string, formatArgs ...interface{}) {
+	printArgs := make([]interface{}, len(formatArgs))
+	copy(printArgs, formatArgs)
+	// Show trace if the debug flag is set
+	if msg == "" || debug {
+		panic(fmt.Sprintf(msg, printArgs...))
+	}
+	fmt.Fprintf(os.Stderr, msg, printArgs...)
+	fmt.Fprint(os.Stderr, "\n")
+	os.Exit(1)
+}
+
+// Process generator file
+func processGeneratorConfig(filePath string) []byte {
+	p := internal.Plugin{}
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		errorAndExit("failed to read file '%s': %s", filePath, err)
+	}
+
+	err = p.Config(fileData)
+	if err != nil {
+		errorAndExit("error parsing config file '%s': %s", filePath, err)
+	}
+
+	generatedOutput, err := p.Generate()
+	if err != nil {
+		errorAndExit("error generating policies from config file '%s': %s", filePath, err)
+	}
+
+	return generatedOutput
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/open-cluster-management/policy-generator-plugin
 go 1.16
 
 require (
+	github.com/spf13/pflag v1.0.5
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 	k8s.io/apimachinery v0.22.1
 )
