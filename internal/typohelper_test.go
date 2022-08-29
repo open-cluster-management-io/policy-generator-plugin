830455d27d109363b0a75e22163f3bfaa2ee2044
Justin Kulikauskas <jkulikau@redhat.com>
2022-08-29 17:11:33 -0400

    Fail generation and add help on field typos
    When the input configuration does not match the schema in the go type,
    the generator will now respond with an error. The intention is to help
    users when there is a typo in the configuration, which otherwise would
    just be ignored (and in the case of a map, any configuration "inside"
    would be dropped).
    
    This also adds help text to these error cases, to try and identify what
    field the user might have meant. That implementation uses a library
    which does not compute the Levenshtein distance, but should work well
    enough, and has the advantage of already being in the import graph.
    
    Refs:
     - https://github.com/stolostron/backlog/issues/19646
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

---
+++ internal/typohelper_test.go
@@ -0,0 +1,170 @@
+package internal
+
+import (
+	"bytes"
+	"fmt"
+	"path"
+	"regexp"
+	"testing"
+)
+
+func TestConfigTypos(t *testing.T) {
+	t.Parallel()
+	tmpDir := t.TempDir()
+	createConfigMap(t, tmpDir, "configmap.yaml")
+
+	suggestFmt := "line %v: field %v found but not defined in type %v - did you mean '%v'?"
+
+	tests := map[string]struct {
+		desiredErrs []string
+		generator   []byte
+	}{
+		"no typos": {
+			desiredErrs: []string{},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-minimal
+policyDefaults:
+  namespace: minimal
+policies:
+- name: my-minimal
+  manifests:
+  - path: @@@
+`),
+		},
+		"one typo with suggestion": {
+			desiredErrs: []string{
+				fmt.Sprintf(suggestFmt, "6", "policyDefault", "PolicyGenerator", "policyDefaults"),
+			},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-minimal
+policyDefault:
+  namespace: minimal
+policies:
+- name: my-minimal
+  manifests:
+  - path: @@@
+`),
+		},
+		"two typos with suggestions": {
+			desiredErrs: []string{
+				fmt.Sprintf(suggestFmt, "6", "policyDefault", "PolicyGenerator", "policyDefaults"),
+				fmt.Sprintf(suggestFmt, "8", "policie", "PolicyGenerator", "policies"),
+			},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-minimal
+policyDefault:
+  namespace: minimal
+policie:
+- name: my-minimal
+  manifests:
+  - path: @@@
+`),
+		},
+		"one deeper typo": {
+			desiredErrs: []string{
+				fmt.Sprintf(suggestFmt, "7", "configPolicyAnnotations", "policyDefaults",
+					"configurationPolicyAnnotations"),
+			},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-minimal
+policyDefaults:
+  configPolicyAnnotations: {}
+  namespace: minimal
+policies:
+- name: my-minimal
+  manifests:
+  - path: @@@
+`),
+		},
+		"typo inside a list": {
+			desiredErrs: []string{
+				fmt.Sprintf(suggestFmt, "11", "paths", "manifests", "path"),
+			},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-minimal
+policyDefaults:
+  namespace: minimal
+policies:
+- name: my-minimal
+  manifests:
+  - paths: @@@
+`),
+		},
+		"typo no suggestion": {
+			desiredErrs: []string{
+				"line 12: field namespace found but not defined in type manifests$",
+			},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: policy-generator-minimal
+policyDefaults:
+  namespace: minimal
+policies:
+- name: my-minimal
+  manifests:
+  - path: @@@
+    namespace: foo
+`),
+		},
+		"non-typo error": {
+			desiredErrs: []string{
+				"line 6: cannot unmarshal !!seq into string",
+			},
+			generator: []byte(`
+apiVersion: policy.open-cluster-management.io/v1
+kind: PolicyGenerator
+metadata:
+  name: 
+  - policy-generator-minimal
+policyDefaults:
+  namespace: minimal
+policies:
+- name: my-minimal
+  manifests:
+  - path: @@@
+`),
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			p := Plugin{}
+			generatorYaml := bytes.ReplaceAll(
+				test.generator,
+				[]byte(`@@@`),
+				[]byte(path.Join(tmpDir, "configmap.yaml")))
+
+			err := p.Config(generatorYaml, tmpDir)
+			if err == nil && len(test.desiredErrs) > 0 {
+				t.Fatal("Expected an error to be emitted, got nil")
+			}
+
+			for _, want := range test.desiredErrs {
+				if match, _ := regexp.MatchString(want, err.Error()); !match {
+					t.Errorf("Expected error to include '%v' - got '%v'", want, err.Error())
+				}
+			}
+		})
+	}
+}
